d0eea04432e399829619a2140c24a0fadec60e20
kkranenberg <karsten94kk@kkranenberg.de>
2020-06-17 17:00:15 +0200

    acled-dl.py finished

---
+++ acled-dl.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+import io
+import json
+import urllib
+print("Import successful!")
+
+print('Starting ACLED import.')
+acledDF = pd.read_csv(io.StringIO(requests.get('https://api.acleddata.com/acled/read.csv?terms=accept&limit=0').content.decode('utf-8')))
+print('Importing ACLED successful!')
+
+acledDF.to_csv('acled_api.csv', index=False)
+print('Wrote ACLED to disk.')
