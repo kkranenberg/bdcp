eadd5514965027356ac777018b8405c9037582d3
kkranenberg <karsten94kk@kkranenberg.de>
2020-06-21 15:30:01 +0200

    reorganized file tree

--- acled-dl.py
+++ acled-dl.py
@@ -6,8 +6,9 @@ import urllib
 print("Import successful!")
 
 print('Starting ACLED import.')
-acledDF = pd.read_csv(io.StringIO(requests.get('https://api.acleddata.com/acled/read.csv?terms=accept&limit=0').content.decode('utf-8')))
-print('Importing ACLED successful!')
+#acledDF = pd.read_csv(io.StringIO(requests.get('https://api.acleddata.com/acled/read.csv?terms=accept&limit=0').content.decode('utf-8')))
+acledDF = pd.read_csv('https://api.acleddata.com/acled/read.csv?terms=accept&limit=0')
+print('Imported ACLED successful!')
 
 acledDF.to_csv('acled_api.csv', index=False)
 print('Wrote ACLED to disk.')
